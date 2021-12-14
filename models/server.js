7103da5a1ae0ae8d0cacf980858c616a9226b1c4
carlos <carlosCACB333@gmail.com>
2021-12-13 19:18:01 -0500

    'v1.0.4'

--- models/server.js
+++ models/server.js
@@ -7,6 +7,7 @@ const { dbconexion } = require('../db/db-config');
 const { checkToken } = require('../helpers/web-token');
 const { connectedUser, disconnectedUser, listUsers, saveMessage, listMessages } = require('../controllers/socket');
 const fileUpload = require('express-fileupload');
+const path = require('path');
 
 class Server {
 
@@ -29,7 +30,10 @@ class Server {
         this.app.use(express.json())
 
         // ruta publica
-        this.app.use(express.static('public'))
+        // this.app.use(express.static('public'))
+        console.log(__dirname);
+        this.app.use(express.static('public'));
+        this.app.get('/*', (req, res) => { res.sendFile(path.join(__dirname, '../public', 'index.html')); });
 
         //configurando carga de archivos 
         this.app.use(fileUpload({
